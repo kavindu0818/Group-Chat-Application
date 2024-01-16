8b3de8eca4e780b88117a17ec82d53e1e3c247b9
kavindu0818 <kavindumaduranga184@gmail.com>
2024-01-16 11:14:54 +0530

    commit

---
+++ src/main/java/org/example/Controller/LoginFormController.java
@@ -0,0 +1,16 @@
+package org.example.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class LoginFormController {
+    public TextField txtUserName;
+    public ImageView imageProfilePicture;
+
+    public void btnOpenBrowserOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnChatStrartOnAction(ActionEvent actionEvent) {
+    }
+}
