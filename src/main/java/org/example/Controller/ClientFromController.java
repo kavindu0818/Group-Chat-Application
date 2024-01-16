0e59989d850c9f25ce1417311284e31c6b23e7d0
kavindu0818 <kavindumaduranga184@gmail.com>
2024-01-16 11:00:36 +0530

    Secound commit

---
+++ src/main/java/org/example/Controller/ClientFromController.java
@@ -0,0 +1,23 @@
+package org.example.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class ClientFromController {
+    public TextArea txtViewChat;
+    public TextField txtTypemessage;
+    public Label lbiUserName;
+    public ImageView imageViewUser;
+
+    public void btnImojiOnAction(ActionEvent actionEvent) {
+    }
+
+    public void bthGalleryOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSendOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/org/example/Controller/ServerFormController.java
@@ -0,0 +1,9 @@
+package org.example.Controller;
+
+import javafx.event.ActionEvent;
+
+public class ServerFormController {
+    public void btnGetStartOnAction(ActionEvent actionEvent) {
+
+    }
+}
